18cea59f829eef57311c059b83f02dbb1898a1db
Claudiu <test@test.com>
2020-04-11 14:08:20 +0200

    initial commit

---
+++ WinUIProblem/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WinUIProblem
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MainViewModel vm = new MainViewModel();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NumberBox_ValueChanged(Microsoft.UI.Xaml.Controls.NumberBox sender, Microsoft.UI.Xaml.Controls.NumberBoxValueChangedEventArgs args)
+        {
+            Debug.WriteLine($"Value changed to {sender.Value}");
+            vm.Amount = (int)sender.Value;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await vm.SendMoney();
+        }
+    }
+}
---
+++ WinUIProblem/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinUIProblem
+{
+    public class MainViewModel : Observable
+    {
+        private int _amount;
+        public int Amount
+        {
+            get => _amount;
+            set => SetProperty(ref _amount, value);
+        }
+
+        public async Task SendMoney()
+        {
+            Debug.WriteLine($"Sending {Amount}");
+            await Task.Delay(1);
+        }
+    }
+}
