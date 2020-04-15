1507b48310e16b68dd83a4770154bc7518172f81
Claudiu <test@test.com>
2020-04-15 18:43:56 +0200

    Update to show ComboBox bug

--- WinUIProblem/MainViewModel.cs
+++ WinUIProblem/MainViewModel.cs
@@ -9,11 +9,27 @@ namespace WinUIProblem
 {
     public class MainViewModel : Observable
     {
+        public Dictionary<string, string> MyItems { get; set; } = new Dictionary<string, string>();
+
+        public MainViewModel()
+        {
+            MyItems.Add("item_1", "ITEM 1");
+            MyItems.Add("item_2", "ITEM 2");
+            SelectedItem = "item_1";
+        }
+
         private int _amount;
         public int Amount
         {
             get => _amount;
             set => SetProperty(ref _amount, value);
+        }
+
+        private string _selectedItem;
+        public string SelectedItem
+        {
+            get => _selectedItem;
+            set => SetProperty(ref _selectedItem, value);
         }
 
         public async Task SendMoney()
